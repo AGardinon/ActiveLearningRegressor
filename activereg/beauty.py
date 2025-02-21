a7a1e0d6f15a903f9057d2f1f3814cfc47fadcd0
AGardinon <andreagardin92@gmail.com>
2025-02-21 10:27:57 +0100

    added beauty module for plots

---
+++ activereg/beauty.py
@@ -0,0 +1,52 @@
+#
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# --- PLOT UTILITIES ---#
+
+def get_alphas(Z: np.ndarray, scale: bool=False, treshold: float=.50001) -> np.ndarray:
+    alphas = (Z.ravel() - Z.ravel().min()) / (Z.ravel().max() - Z.ravel().min())
+    if scale:
+        for i,av in enumerate(alphas):
+            if av <= treshold:
+                alphas[i] = 0.
+            else:
+                pass
+    return alphas
+
+
+def get_axes(plots: int, 
+             max_col: int =2, 
+             fig_frame: tuple =(3.3,3.), 
+             res: int =200):
+    """Define Fig and Axes objects.
+    """
+    # cols and rows definitions
+    cols = plots if plots <= max_col else max_col
+    rows = int(plots / max_col) + int(plots % max_col != 0)
+
+    fig, axes = plt.subplots(rows,
+                             cols,
+                             figsize=(cols * fig_frame[0], rows * fig_frame[1]),
+                             dpi=res)
+    if plots > 1:
+        axes = axes.flatten()
+        for i in range(plots, max_col*rows):
+            remove_frame(axes[i])
+    elif plots == 1:
+        pass
+    
+    return fig, axes
+
+
+def remove_frame(axes) -> None:
+    for side in ['bottom', 'right', 'top', 'left']:
+        axes.spines[side].set_visible(False)
+    axes.set_yticks([])
+    axes.set_xticks([])
+    axes.xaxis.set_ticks_position('none')
+    axes.yaxis.set_ticks_position('none')
+    pass
+
+# --- ////////////// ---#

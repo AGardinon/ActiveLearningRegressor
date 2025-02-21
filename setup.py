6c3462bfbc7a9508cbcaeb9977795cb40df77124
AGardinon <andreagardin92@gmail.com>
2025-02-21 10:20:39 +0100

    added pip setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='activereg',
+    version='0.1.0',
+    packages=find_packages(),
+    url='https://github.com/AGardinon/ActiveLearningRegressor',
+    author='Andrea Gardin',
+    description='Active regressor tool for supervised learing problems.',
+)

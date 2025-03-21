3cbbce7d576490d6e2ff91cae4a3385af3f205be
AGardinon <andreagardin92@gmail.com>
2025-03-21 15:48:15 +0100

    reworked to modular script for running 2d test experiments

--- examples/run_2dexample.py
+++ examples/run_2dexample.py
@@ -1,101 +1,67 @@
 #!
-
+import toml
+import argparse
 import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.gaussian_process import GaussianProcessRegressor
-from sklearn.gaussian_process.kernels import RBF, Matern, WhiteKernel, ConstantKernel as C
 from activereg.sampling import sample_landscape
-from activereg.acquisition import landscape_acquisition, highest_landscape_selection
-from activereg.beauty import get_axes
-
-# Test experiment setup
-OUTDIR = './test1/'
-X_pool = np.load('./data/point_space_2d_scaled.npy')
-y_pool = np.load('./data/proptest2d_2.npy')
-
-# Regressor setup
-kernel = Matern(length_scale=1.0, nu=2.5)
-gpr = GaussianProcessRegressor(kernel=kernel, n_restarts_optimizer=10)
+from activereg.cycle import active_learning_cycle
+from activereg.mlmodel import ML_MODELS
+from activereg.beauty import plot_2Dcycle
+from activereg.format import EXAMPLES_REPO
 
 # - func
 
-def plot_cycles(train_set, next_set, pool_set, pred_set, landscape_set, name_set, show=False):
-    fig, ax = get_axes(3,3)
-
-    X,y,cmap = pool_set
-    Xc,yp = pred_set
-    Xt,yt = train_set
-    Xn,yn = next_set
-    landsc,hls,cmapl = landscape_set
-
-    ax[0].scatter(*Xc.T,c=yp,s=5,cmap=cmap,vmin=min(y),vmax=max(y))
-    ax[0].scatter(*Xt.T,c=yt,s=30,cmap=cmap,vmin=min(y),vmax=max(y),marker='o',edgecolor='black',zorder=3)
-    ax[0].scatter(*Xn.T,c=yn,s=30,marker='*',edgecolor='black',zorder=3)
-
-    ax[1].scatter(*Xc.T,c=landsc,s=5,cmap=cmapl,vmin=min(landsc),vmax=max(landsc))
-    ax[1].scatter(*hls.T,c='.5',s=5,alpha=.3)
-    ax[1].scatter(*Xt.T,s=30,c='0.',marker='o',edgecolor='black',zorder=3)
-    ax[1].scatter(*Xn.T,s=30,c='0.',marker='*',edgecolor='black',zorder=3)
-
-    ax[2].scatter(*X.T,c=y,s=5,cmap=cmap,vmin=min(y),vmax=max(y))
-    ax[2].scatter(*Xt.T,c=yt,s=30,cmap=cmap,vmin=min(y),vmax=max(y),marker='o',edgecolor='black',zorder=3)
-
-    fig.tight_layout()
-    fig_name_list = [str(i) for i in name_set]
-    fig_name = fig_name_list[0]+"_".join(fig_name_list[1:])
-    fig.savefig(fig_name+'.png')
-    if show:
-        plt.show()
-
-# - main
-
-def main(n_batch: int=4, 
-         init_sampling_mode: str='fps',
-         n_cycles: int=8,
-         acquisition_mode: str='explore_uncertainty',
-         sampling_mode: str='voronoi'):
+def run_2Dexperiment(
+        X_pool,
+        y_pool,
+        ml_model,
+        n_batch: int,
+        init_sampling_mode: str,
+        n_cycles: int,
+        acquisition_mode: str,
+        percentile: int,
+        sampling_mode: str,
+        out_dir: str):
 
     # 1. init experiment
     # Sample initial points
-    init_idx = sample_landscape(X_landscape=X_pool, 
-                                n_points=n_batch,
-                                mode=init_sampling_mode)
+    if isinstance(init_sampling_mode, str):
+        init_idx = sample_landscape(X_landscape=X_pool, 
+                                    n_points=n_batch,
+                                    sampling_mode=init_sampling_mode)
+        init_sampling_mode_str = init_sampling_mode
+    elif isinstance(init_sampling_mode, list):
+        init_idx = init_sampling_mode
+        init_sampling_mode_str = 'predef'
     
     X_train, y_train = X_pool[init_idx], y_pool[init_idx]
     X_candidates = np.delete(X_pool, init_idx, axis=0)
     y_candidates = np.delete(y_pool, init_idx, axis=0)
 
     # Model training on initial configuration
-    gpr.fit(X_train, y_train)
+    ml_model.fit(X_train, y_train)
 
     # 2. cycles
     for c in range(n_cycles):
 
         print(f'Cycle: {c+1}')
 
-        # 3. compute the navigation landscape
-        y_pred, landscape = landscape_acquisition(X_candidates=X_candidates,
-                                          gp_model=gpr,
-                                          acquisition_mode=acquisition_mode)
-        # get the indexes of the top percentile based on the current candidates
-        acq_landscape_ndx = highest_landscape_selection(landscape=landscape, percentile=80)
-        X_acq_landscape = X_candidates[acq_landscape_ndx]
-        
-        # 4. sample new points in from the landscape
-        sampled_hls_idx = sample_landscape(X_landscape=X_acq_landscape, 
-                                           n_points=n_batch, 
-                                           sampling_mode=sampling_mode)
-        sampled_new_idx = acq_landscape_ndx[sampled_hls_idx]
-
-        X_next, y_next = X_candidates[sampled_new_idx], y_candidates[sampled_new_idx]
+        X_next, y_next, y_pred, landscape, X_acq_landscape, sampled_new_idx = active_learning_cycle(
+            X_candidates=X_candidates,
+            y_candidates=y_candidates,
+            model=ml_model,
+            acquisition_mode=acquisition_mode,
+            percentile=percentile,
+            n_batch=n_batch,
+            sampling_mode=sampling_mode
+        )
 
         # - plt
-        plot_cycles(train_set=(X_train,y_train),
+        plot_2Dcycle(train_set=(X_train,y_train),
                     pred_set=(X_candidates,y_pred),
                     pool_set=(X_pool,y_pool,'coolwarm'),
                     next_set=(X_next,y_next),
                     landscape_set=(landscape,X_acq_landscape,'plasma'),
-                    name_set=(OUTDIR,'fig',init_sampling_mode,acquisition_mode,sampling_mode,c))
+                    name_set=(out_dir,'fig_',init_sampling_mode_str,acquisition_mode,sampling_mode,c))
 
         # 5. update the trainig set
         X_train = np.vstack((X_train, X_next))
@@ -106,8 +72,29 @@ def main(n_batch: int=4,
         y_candidates = np.delete(y_candidates, sampled_new_idx, axis=0)
 
         # 6. retrain ALmodel with new points
-        gpr.fit(X_train, y_train)
+        ml_model.fit(X_train, y_train)
 
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(description="Read a TOML config file.")
+    parser.add_argument("-c", "--config", required=True, help="Path to the TOML configuration file")
+    args = parser.parse_args()
+    config = toml.load(args.config)
+
+    X_pool = np.load(EXAMPLES_REPO / 'data' / config['X_pool'])
+    y_pool = np.load(EXAMPLES_REPO / 'data' / config['y_pool'])
+
+    out_dir = EXAMPLES_REPO / config['out_dir']
+    out_dir.mkdir(exist_ok=config['overwrite'])
+
+    with open(out_dir / 'config.log', "w") as f:
+        toml.dump(config, f)
+
+    ml_model_type = config['ml_model']
+    ml_model = ML_MODELS[ml_model_type]
+
+    run_2Dexperiment(X_pool=X_pool, 
+                     y_pool=y_pool,
+                     ml_model=ml_model, 
+                     out_dir=out_dir,
+                     **config['experiment'])

5dc789b181b528d7e3b7dfc25f17b875fabef053
AGardinon <andreagardin92@gmail.com>
2025-03-21 15:44:14 +0100

    added plot func for 2d cycles

--- activereg/beauty.py
+++ activereg/beauty.py
@@ -1,9 +1,46 @@
-#
+#!
 
 import numpy as np
 import matplotlib.pyplot as plt
 
-# --- PLOT UTILITIES ---#
+# ---------------------------------------------------------------------------
+# --- PLOT FUNC
+
+
+def plot_2Dcycle(train_set, next_set, pool_set, pred_set, landscape_set, name_set, show=False):
+    fig, ax = get_axes(3,3)
+
+    X,y,cmap = pool_set
+    Xc,yp = pred_set
+    Xt,yt = train_set
+    Xn,yn = next_set
+    landsc,hls,cmapl = landscape_set
+
+    ax[0].scatter(*Xc.T,c=yp,s=5,cmap=cmap,vmin=min(y),vmax=max(y))
+    ax[0].scatter(*Xt.T,c=yt,s=30,cmap=cmap,vmin=min(y),vmax=max(y),marker='o',edgecolor='black',zorder=3)
+    ax[0].scatter(*Xn.T,c=yn,s=30,marker='*',edgecolor='black',zorder=3)
+
+    ax[1].scatter(*Xc.T,c=landsc,s=5,cmap=cmapl,vmin=min(landsc),vmax=max(landsc))
+    ax[1].scatter(*hls.T,c='.5',s=5,alpha=.3)
+    ax[1].scatter(*Xt.T,s=30,c='0.',marker='o',edgecolor='black',zorder=3)
+    ax[1].scatter(*Xn.T,s=30,c='0.',marker='*',edgecolor='black',zorder=3)
+
+    ax[2].scatter(*X.T,c=y,s=5,cmap=cmap,vmin=min(y),vmax=max(y))
+    ax[2].scatter(*Xt.T,c=yt,s=30,cmap=cmap,vmin=min(y),vmax=max(y),marker='o',edgecolor='black',zorder=3)
+
+    fig.tight_layout()
+    fig_name_list = [str(i) for i in name_set[1:]]
+    fig_name = fig_name_list[0]+"_".join(fig_name_list[1:])
+    fig_name = fig_name+'.png'
+    fig.savefig(name_set[0] / fig_name)
+    if show:
+        plt.show()
+
+    return fig, ax
+
+# ---------------------------------------------------------------------------
+# --- PLOT UTILITIES
+
 
 def get_alphas(Z: np.ndarray, scale: bool=False, treshold: float=.50001) -> np.ndarray:
     alphas = (Z.ravel() - Z.ravel().min()) / (Z.ravel().max() - Z.ravel().min())

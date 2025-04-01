2f6775519b7cd8edaa2ffe42b2b2d151d8f9f239
AGardinon <andreagardin92@gmail.com>
2025-04-01 13:16:00 +0200

    switched to yaml and adapted to new ml class the test run code

--- examples/run_2dexample.py
+++ examples/run_2dexample.py
@@ -1,10 +1,9 @@
 #!
-import toml
+import yaml
 import argparse
 import numpy as np
 from activereg.sampling import sample_landscape
 from activereg.cycle import active_learning_cycle
-from activereg.mlmodel import ML_MODELS
 from activereg.beauty import plot_2Dcycle
 from activereg.format import EXAMPLES_REPO
 
@@ -38,7 +37,7 @@ def run_2Dexperiment(
     y_candidates = np.delete(y_pool, init_idx, axis=0)
 
     # Model training on initial configuration
-    ml_model.fit(X_train, y_train)
+    ml_model.train(X_train, y_train)
 
     # 2. cycles
     for c in range(n_cycles):
@@ -72,29 +71,47 @@ def run_2Dexperiment(
         y_candidates = np.delete(y_candidates, sampled_new_idx, axis=0)
 
         # 6. retrain ALmodel with new points
-        ml_model.fit(X_train, y_train)
+        ml_model.train(X_train, y_train)
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="Read a TOML config file.")
-    parser.add_argument("-c", "--config", required=True, help="Path to the TOML configuration file")
+    # Argument parser
+    parser = argparse.ArgumentParser(description="Read a YAML config file.")
+    parser.add_argument("-c", "--config", required=True, help="Path to the YAML configuration file")
     args = parser.parse_args()
-    config = toml.load(args.config)
 
-    X_pool = np.load(EXAMPLES_REPO / 'data' / config['X_pool'])
-    y_pool = np.load(EXAMPLES_REPO / 'data' / config['y_pool'])
+    # Load YAML file
+    with open(args.config, "r") as file:
+        config = yaml.safe_load(file)  # Use safe_load instead of load
 
-    out_dir = EXAMPLES_REPO / config['out_dir']
-    out_dir.mkdir(exist_ok=config['overwrite'])
+    # Split the config files
+    # 1. data
+    data_config = config['data']
+
+    X_pool = np.load(EXAMPLES_REPO / 'data' / data_config['X_pool'])
+    y_pool = np.load(EXAMPLES_REPO / 'data' / data_config['y_pool'])
+    
+    out_dir = EXAMPLES_REPO / data_config['out_dir']
+    out_dir.mkdir(exist_ok=data_config['overwrite'])
 
     with open(out_dir / 'config.log', "w") as f:
-        toml.dump(config, f)
+        yaml.dump(config, f, default_flow_style=False)
 
-    ml_model_type = config['ml_model']
-    ml_model = ML_MODELS[ml_model_type]
+    # 2. ml
+    ml_config = config['ml']
+    print(ml_config)
 
+    if ml_config['ml_model'] == 'GPR':
+        from activereg.mlmodel import GPR, KernelFactory
+
+        kernel = KernelFactory(kernel_recipe=ml_config['kernel_recipe']).get_kernel()
+        ml_model = GPR(kernel=kernel, **ml_config['ml_model_param'])
+        print(ml_model)
+
+    # 3. experiment
+    exp_config = config['experiment']
     run_2Dexperiment(X_pool=X_pool, 
                      y_pool=y_pool,
                      ml_model=ml_model, 
                      out_dir=out_dir,
-                     **config['experiment'])
+                     **exp_config)

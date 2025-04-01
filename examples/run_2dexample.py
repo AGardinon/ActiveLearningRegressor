4feccd1261e3e492d786241abd350937ee19eb90
AGardinon <andreagardin92@gmail.com>
2025-04-01 17:35:46 +0200

    added report logs

--- examples/run_2dexample.py
+++ examples/run_2dexample.py
@@ -1,10 +1,14 @@
 #!
 import yaml
+import json
 import argparse
 import numpy as np
+import pandas as pd
+from pathlib import Path
 from activereg.sampling import sample_landscape
 from activereg.cycle import active_learning_cycle
 from activereg.beauty import plot_2Dcycle
+from activereg.utils import create_experiment_name
 from activereg.format import EXAMPLES_REPO
 
 # - func
@@ -39,9 +43,20 @@ def run_2Dexperiment(
     # Model training on initial configuration
     ml_model.train(X_train, y_train)
 
+    # report
+    X_sampled = []
+    y_sampled = []
+    cycles_label = []
+    cycle_predictions = []
+
+    X_sampled.append(X_train)
+    y_sampled.append(y_train)
+    cycles_label.append([0]*n_batch)
+
     # 2. cycles
     for c in range(n_cycles):
         print(f'Cycle: {c+1}')
+
         y_best = max(y_train)
 
         X_next, y_next, y_pred, landscape, X_acq_landscape, sampled_new_idx = active_learning_cycle(
@@ -65,16 +80,50 @@ def run_2Dexperiment(
                               acquisition_parameters['acquisition_mode'],sampling_mode,c)
                     )
 
-        # 5. update the trainig set
+        # 3. report
+        X_sampled.append(X_next)
+        y_sampled.append(y_next)
+        cycles_label.append([c+1]*n_batch)
+        cycle_predictions.append(y_pred)
+
+        # 4. update the trainig set
         X_train = np.vstack((X_train, X_next))
         y_train = np.append(y_train, y_next)
 
-        # remove selected point from candidates
+        # 5. remove selected point from candidates
         X_candidates = np.delete(X_candidates, sampled_new_idx, axis=0)
         y_candidates = np.delete(y_candidates, sampled_new_idx, axis=0)
 
         # 6. retrain ALmodel with new points
         ml_model.train(X_train, y_train)
+
+    # 7. outputs
+    exp_name = create_experiment_name(name_set=(
+        'report_',acquisition_parameters['acquisition_mode'],sampling_mode,
+        f'nb{n_batch}',f'ncy{n_cycles}')
+        )
+
+    X_sampled = np.concatenate(X_sampled)
+    al_report_df_dict = {f'X_s{v}' : X_sampled[:,v] for v in range(X_sampled.shape[1])}
+    al_report_df_dict['y_s'] = np.concatenate(y_sampled)
+    al_report_df_dict['cycles_label'] = np.concatenate(cycles_label)
+    al_report_df = pd.DataFrame(al_report_df_dict)
+    al_report_df.to_csv(out_dir / Path(exp_name+'_exp.csv'))
+
+    al_report_dict = {
+        'ml_model' : ml_model.__repr__(),
+        'n_cycles' : n_cycles,
+        'n_batch' : n_batch,
+        'init_sampling_mode' : init_sampling_mode,
+        'acquisition_parameters' : acquisition_parameters,
+        'percentile' : percentile,
+        'sampling_mode' : sampling_mode,
+    }
+    # outputs
+    with open(out_dir / Path(exp_name+"_exp_report.json"), "w") as f:
+        json.dump(al_report_dict, f, indent=4)
+
+    return exp_name
 
 
 if __name__ == '__main__':
@@ -97,9 +146,6 @@ if __name__ == '__main__':
     out_dir = EXAMPLES_REPO / data_config['out_dir']
     out_dir.mkdir(exist_ok=data_config['overwrite'])
 
-    with open(out_dir / 'config.log', "w") as f:
-        yaml.dump(config, f, default_flow_style=False)
-
     # 2. ml
     ml_config = config['ml']
 
@@ -112,8 +158,9 @@ if __name__ == '__main__':
 
     # 3. experiment
     exp_config = config['experiment']
-    run_2Dexperiment(X_pool=X_pool, 
-                     y_pool=y_pool,
-                     ml_model=ml_model, 
-                     out_dir=out_dir,
-                     **exp_config)
+    exp_name = run_2Dexperiment(X_pool=X_pool, y_pool=y_pool, 
+                                ml_model=ml_model, out_dir=out_dir, 
+                                **exp_config)
+
+    with open(out_dir / Path(exp_name+'_config.log'), "w") as f:
+        yaml.dump(config, f, default_flow_style=False)

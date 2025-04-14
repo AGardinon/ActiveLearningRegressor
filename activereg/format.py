95dfa9be5e51e9ccf16dc31b52adce7d2124809a
AGardinon <andreagardin92@gmail.com>
2025-04-14 16:55:02 +0200

    added file formats for two outputs

--- activereg/format.py
+++ activereg/format.py
@@ -6,3 +6,7 @@ REPO_ROOT = Path(__file__).resolve().parents[1]
 
 # dir for the examples
 EXAMPLES_REPO = REPO_ROOT / 'examples'
+
+# file formats
+FILE_TO_VAL = 'X_next_cycle_{}.csv'
+FILE_VALIDATED = 'X_next_cycle_{}_validated.csv'

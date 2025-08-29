1e321fc0c79142b2c4f629e47619181c08a2961c
AGardinon <andreagardin92@gmail.com>
2025-08-29 14:40:29 +0200

    added helper function to set up experiment related variables

--- activereg/experiment.py
+++ activereg/experiment.py
@@ -3,12 +3,37 @@
 import numpy as np
 import pandas as pd
 import activereg.mlmodel as regmodels
+from activereg.format import DATASETS_REPO
 from activereg.sampling import sample_landscape
+from activereg.hyperparams import get_gp_kernel
 from activereg.acquisition import (
     penalize_landscape_fast,
     highest_landscape_selection,
     AcquisitionFunction,
 )
+
+
+def get_gt_dataframes(config: dict) -> tuple[pd.DataFrame, pd.DataFrame]:
+    """Gets the ground truth and evidence dataframes from the config.
+
+    Args:
+        config (dict): Configuration dictionary containing paths to the dataframes.
+
+    Returns:
+        tuple[pd.DataFrame, pd.DataFrame]: Ground truth dataframe and evidence dataframe.
+    """
+    gt_df_name = config.get('ground_truth_file', None)
+    if gt_df_name is None:
+        raise ValueError("Ground truth dataframe name must be provided in the config file.")
+    gt_df = pd.read_csv(DATASETS_REPO / gt_df_name)
+
+    exp_evidence_df_name = config.get('experiment_evidence', None)
+    if exp_evidence_df_name is not None:
+        evidence_df = pd.read_csv(DATASETS_REPO / exp_evidence_df_name)
+    else:
+        evidence_df = None
+
+    return gt_df, evidence_df
 
 
 def setup_data_pool(df: pd.DataFrame, search_var: list[str], scaler: str) -> tuple[np.ndarray, any]:
@@ -42,6 +67,79 @@ def setup_data_pool(df: pd.DataFrame, search_var: list[str], scaler: str) -> tup
     X_scaled_array = scaler.fit_transform(X)
 
     return X_scaled_array, scaler
+
+
+def setup_experiment_variables(config: dict) -> tuple[str, str, int, int, str, str, list[dict]]:
+    """Sets up the experiment parameters from the config dictionary.
+
+    Args:
+        config (dict): Configuration dictionary containing experiment parameters.
+
+    Returns:
+        tuple: Contains experiment name, additional notes, number of cycles, initial batch size,
+               initial sampling method, cycle sampling method, and acquisition parameters.
+    """
+    exp_name = config.get('experiment_name', 'Insilico_AL_Simulation')
+    additional_notes = config.get('experiment_notes', '')
+    n_cycles = config.get('n_cycles', 3)
+    init_batch = config.get('init_batch_size', 8)
+    init_sampling = config.get('init_sampling', 'fps')
+    cycle_sampling = config.get('cycle_sampling', 'voronoi')
+    acquisition_params = config.get('acquisition_parameters', [])
+
+    search_space_vars = config.get('search_space_variables', [])
+    assert len(search_space_vars) > 0, "Search space variables must be defined in the config file."
+
+    target_vars = config.get('target_variables', None)
+    assert target_vars is not None, "Target variables must be defined in the config file."
+
+    return (exp_name, 
+            additional_notes, 
+            n_cycles, 
+            init_batch, 
+            init_sampling, 
+            cycle_sampling, 
+            acquisition_params,
+            search_space_vars,
+            target_vars)
+
+
+def setup_ml_model(config: dict) -> regmodels.MLModel:
+    """Sets up the machine learning model based on the configuration.
+
+    Args:
+        config (dict): Configuration dictionary containing model parameters.
+
+    Returns:
+        regmodels.MLModel: The configured machine learning model.
+    """
+    ml_model_type = config.get('ml_model', None)
+    assert ml_model_type is not None, "ML model type must be specified in the config file."
+
+    if ml_model_type == 'GPR':
+        return create_gpr_instance(config)
+    else:
+        raise ValueError(f"Unknown ML model type: {ml_model_type}. Supported types are: ['GPR'].")
+
+
+def create_gpr_instance(config: dict) -> regmodels.MLModel:
+    """Creates a Gaussian Process Regressor instance.
+
+    Args:
+        config (dict): Configuration dictionary containing model parameters.
+
+    Returns:
+        regmodels.MLModel: The created Gaussian Process Regressor instance.
+    """
+    kernel_recipe = config.get('kernel_recipe', None)
+    assert kernel_recipe is not None, "Kernel recipe must be specified for GPR in the config file."
+
+    kernel_recipe = get_gp_kernel(kernel_recipe)
+
+    model_parameters = config.get('model_parameters', {})
+    model_parameters['alpha'] = float(model_parameters['alpha'])
+
+    return regmodels.GPR(kernel=kernel_recipe, **model_parameters)
 
 
 def sampling_block(
